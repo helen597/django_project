ef12c105beb3181345766bf3e36dc188d7c2a93b
Olga Semenova <helen597597@gmail.com>
2024-02-12 03:07:35 +0500

    get contacts

--- catalog/views.py
+++ catalog/views.py
@@ -7,4 +7,11 @@ def home(request):
 
 
 def contacts(request):
+    if request.method == 'POST':
+        # в переменной request хранится информация о методе, который отправлял пользователь
+        name = request.POST.get('name')
+        # а также передается информация, которую заполнил пользователь
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(f"{name}({phone}): {message}")
     return render(request, 'catalog/contacts.html')
