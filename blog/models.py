3c8ab5bae26110f35bdafdfc5aa4b8d156155e6b
Olga Semenova <helen597597@gmail.com>
2024-03-04 02:39:18 +0500

    create model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Заголовок')
+    slug = models.CharField(max_length=150, verbose_name='slug', null=True, blank=True)
+    content = models.TextField(max_length=300, verbose_name='Содержимое')
+    image = models.ImageField(upload_to='blogs/', verbose_name='Изображение', null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
+    view_count = models.IntegerField(default=0, verbose_name='Количество просмотров')
+
+    def __str__(self):
+        return f'{self.title}, {self.created_at}'
+
+    class Meta:
+        verbose_name = 'запись блога'
+        verbose_name_plural = 'записи блога'
+        ordering = ('created_at',)
