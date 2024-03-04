95fbd242506588c8b2c98670e361da1259e78607
Olga Semenova <helen597597@gmail.com>
2024-03-04 23:12:31 +0500

    edit views

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
+from django.utils.text import slugify
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from blog.models import Blog
@@ -8,6 +9,11 @@ from blog.models import Blog
 # Create your views here.
 class BlogListView(ListView):
     model = Blog
+
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset(*args, **kwargs)
+        queryset = queryset.filter(is_published=True)
+        return queryset
 
 
 class BlogDetailView(DetailView):
@@ -23,15 +29,22 @@ class BlogDetailView(DetailView):
 class BlogCreateView(CreateView):
     model = Blog
     success_url = reverse_lazy('blog:blog_list')
-    fields = '__all__'
+    fields = ('title', 'content', 'image',)
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_blog = form.save()
+            new_blog.slug = slugify(new_blog.title)
+            new_blog.save()
+        return super().form_valid(form)
 
 
 class BlogUpdateView(UpdateView):
     model = Blog
-    fields = ('title', 'content')
+    fields = ('title', 'content', 'image', 'is_published',)
 
     def get_success_url(self):
-        return reverse_lazy('blog:blog_detail', kwargs={'pk': self.object.pk})
+        return reverse_lazy('blog:blog_list')
 
 
 class BlogDeleteView(DeleteView):
