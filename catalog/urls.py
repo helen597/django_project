8dac5da1aa9b4a5dbe4ec0a4ecd090e8979e54a9
Olga Semenova <helen597597@gmail.com>
2024-03-18 00:40:36 +0500

    edit views, templates

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 
-from catalog.views import ProductListView, ProductDetailView, ContactsTemplateView
+from catalog.views import ProductListView, ProductDetailView, ContactsTemplateView, ProductCreateView, ProductUpdateView
 from catalog import apps
 
 app_name = apps.CatalogConfig.name
 
 urlpatterns = [
-    path('', ProductListView.as_view(), name='home'),
-    path('contacts/', ContactsTemplateView.as_view(), name='contacts'),
-    path('product/<int:pk>/', ProductDetailView.as_view(), name='product')
+    path('', ProductListView.as_view(), name='product_list'),
+    path('catalog/contacts/', ContactsTemplateView.as_view(), name='contacts'),
+    path('catalog/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('catalog/create/', ProductCreateView.as_view(), name='product_create'),
+    path('catalog/<int:pk>/update/', ProductUpdateView.as_view(), name='product_update')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,6 @@
+from django import forms
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, TemplateView, CreateView, UpdateView, DeleteView
 
 from catalog.models import Product
@@ -7,12 +9,32 @@ from catalog.models import Product
 # Create your views here.
 class ProductListView(ListView):
     model = Product
-    template_name = 'catalog/home.html'
+    template_name = 'catalog/product_list.html'
 
 
 class ProductDetailView(DetailView):
     model = Product
-    template_name = 'catalog/product.html'
+    template_name = 'catalog/product_detail.html'
+
+
+class ProductForm(forms.ModelForm):
+    # Наследуемся от специального класса форм, который предоставляет
+    # весь необходимый функционал, который нужно настроить
+    class Meta:
+        model = Product
+        exclude = ('created_at', 'updated_at', )
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    success_url = reverse_lazy('catalog:product_list')
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductForm
+    success_url = reverse_lazy('catalog:product_list')
 
 
 class ContactsTemplateView(TemplateView):
