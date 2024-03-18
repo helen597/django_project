4d2ea4dd1d32626b12e6670e80265dbeea3b071c
Olga Semenova <helen597597@gmail.com>
2024-03-18 23:55:21 +0500

    add style

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,9 +1,20 @@
 from django import forms
+from django.forms.fields import BooleanField
 
 from catalog.models import Product, Version
 
 
-class ProductForm(forms.ModelForm):
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if isinstance(field, BooleanField):
+                field.widget.attrs['class'] = 'form-check-input'
+            else:
+                field.widget.attrs['class'] = 'form-control'
+
+
+class ProductForm(StyleFormMixin, forms.ModelForm):
     # Наследуемся от специального класса форм, который предоставляет
     # весь необходимый функционал, который нужно настроить
     class Meta:
@@ -31,7 +42,7 @@ class ProductForm(forms.ModelForm):
         return cleaned_data
 
 
-class VersionForm(forms.ModelForm):
+class VersionForm(StyleFormMixin, forms.ModelForm):
     class Meta:
         model = Version
         # exclude = ('product', )
