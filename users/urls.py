054840c67637a8909ae6b780f85ce38082409a11
Olga Semenova <helen597597@gmail.com>
2024-03-25 02:16:25 +0500

    add logout, register

--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,15 @@
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from catalog.views import ProductListView, ProductDetailView, ContactsTemplateView, ProductCreateView, \
     ProductUpdateView, ProductDeleteView
 from users import apps
+from users.views import RegisterView
 
 app_name = apps.UsersConfig.name
 
 urlpatterns = [
-    path('', LoginView.as_view(template_name='users/login.html'), name='login')
-
+    path('', LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', RegisterView.as_view(), name='register')
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from users.forms import UserRegisterForm
+from users.models import User
+
 
 # Create your views here.
+class RegisterView(CreateView):
+    model = User
+    form_class = UserRegisterForm
+    template_name = 'users/register.html'
+    success_url = reverse_lazy('users:login')
